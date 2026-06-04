3f3ca1db8f49fa19a8d10e4d19248d88a6c93415
Cole Wilson <colewilson2004@gmail.com>
2026-06-04 09:42:56 -0500

    Add: find-median-in-a-data-stream - submission-1

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-1.py
@@ -0,0 +1,24 @@
+class MedianFinder:
+
+    def __init__(self):
+        #init empty array
+        self.arr = []
+
+    def addNum(self, num: int) -> None:
+        #adding num to arr
+        self.arr.append(num)
+
+    def findMedian(self) -> float:
+        #start by sorting to find acc MedianFinder#then get len and mid
+        self.arr.sort()
+        n = len(self.arr)
+        mid = n // 2
+
+        #check to see if median is even, if it is return sum of two mid numbers
+        if n % 2 == 0:
+            return (self.arr[mid - 1] + self.arr[mid]) / 2.0
+            
+        # if odd return mid number
+        return float(self.arr[mid])
+            
+        
